022abab6b5a841b2b669ecea42e4fdbc586e9c3a
David Peter <david.a.peter@gmail.com>
2012-07-07 14:42:32 -0400

    At last; 3d works.

--- 3djulia.js
+++ 3djulia.js
@@ -13,23 +13,30 @@ function init() {
     gl.linkProgram(prog);
     gl.useProgram(prog);
 
-    var posAtrLoc = gl.getAttribLocation(prog, "vPos");
+    var posAtrLoc = gl.getAttribLocation(prog, "position");
     gl.enableVertexAttribArray(posAtrLoc); // what does this do again?
     var posBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
     var vertices = new Float32Array([
-        -1,-1,0, 1, -1, 0, 1, 1, 0, -1, 1, 0]);
+        -1,-1,1.,   1, -1, 1.,   1, 1, 1.,   -1, 1, 1.]);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
     gl.vertexAttribPointer(posAtrLoc, 3, gl.FLOAT, false, 0, 0); // wut?
-    cLoc = gl.getUniformLocation(prog, "c");
     gl.uniform4f(gl.getUniformLocation(prog, "scale"), size/2, size/3, size/2, size/3);
-    // anim();
-    draw();
+    gl.uniform3f(gl.getUniformLocation(prog, "rO"), 0, 0, 1.95);
+    gl.uniform1f(gl.getUniformLocation(prog, "epsilon"), 0.006);
+    gl.uniform1i(gl.getUniformLocation(prog, "maxIterations"), 8);
+    gl.uniform3f(gl.getUniformLocation(prog, "light"), -10, -10, -10);
+    gl.uniform4f(gl.getUniformLocation(prog, "backgroundColor"), .2, .2, .2, 1);
+    gl.uniform4f(gl.getUniformLocation(prog, "mu"), -1, 0.4, 0, 0);
+    gl.uniform1i(gl.getUniformLocation(prog, "renderShadows"), 0);
+    anim();
 }
 
 function anim() {
     var t = (+new Date) / 1000;
-    gl.uniform4f(cLoc, -.113 + Math.cos(t) / 2.4, -0.579, -.113 + Math.cos(t) / 2.4, -0.579);
+    gl.uniform4f(gl.getUniformLocation(prog, "mu"),
+        Math.cos(t), 0.4,
+        0, 0);
     draw();
     requestAnimFrame(anim);
 }
